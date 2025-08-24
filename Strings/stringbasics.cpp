387505a869512eb185d35151543dcfea5c083068
namasvigarg <gargnamasvi@gmail.com>
2025-08-24 12:19:50 +0530

    More operations

--- Strings/stringbasics.cpp
+++ Strings/stringbasics.cpp
@@ -7,6 +7,7 @@ int main() {
     string t = " World";
     string c = s+t;
     cout << c << endl;
+    cout << c.at(7); 
     cout << "Length of string: " << c.length() << endl;
     cout << "First letter: " << c[0];
     return 0;
