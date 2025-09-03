0e2ebe8bfb49eafd58ab02ff7a1f2e871fb2b075
Namasvi Garg <namasviwork@gmail.com>
2025-09-03 19:42:27 +0530

    Length of LL

--- Linked List/Linkedlistbasics.cpp
+++ Linked List/Linkedlistbasics.cpp
@@ -44,13 +44,52 @@ int main() {
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct Nodes{
     int val;
-    Node* next;
+    Nodes* next;
 
-    Node(int data1) {
+    Nodes(int data1) {
         val = data1;
         next = nullptr;
     }
 };
+
+Nodes* len(vector<int> &arr) {
+    Nodes* head = new Nodes(arr[0]);
+    Nodes* mover = head;
+
+    for(int i=1 ; i<arr.size() ; i++) {
+        Nodes* temp = new Nodes(arr[i]);
+        mover -> next = temp;
+        mover = temp;
+    }
+    return head;
+}
+
+int getLength(Nodes* head) {
+    int cnt = 0;
+    Nodes* temp = head;
+
+    while(temp != NULL) {
+        cnt++;
+        temp = temp -> next;
+    }
+    return cnt;
+}
+
+int main() {
+    vector<int> arr = {1,3,4,9,34,78};
+    Nodes* head = len(arr);
+    int res = getLength(head);
+
+    cout << "Length of the linked list is: " << res;
+    return 0;
+}
+
+// Insertion at the beginning (head) of a linked list
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
 
